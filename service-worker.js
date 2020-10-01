0c8f453ed499bfaff85392166a8807aea414a512
CristiansSans <cristiansleonardo@gmail.com>
2020-09-30 22:33:56 -0400

    fix

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c66c8981e1fa52c86d553d71a5ad0a15.js"
+  "/precache-manifest.73e1703a64b0ab32a080b1ce4e627867.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
