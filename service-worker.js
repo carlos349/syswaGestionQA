7195e2a0ea4ad251b46bc4e143c52ee630e2d618
Cristians Sanchez <cristiansleonardo@gmail.com>
2022-02-25 23:59:07 -0400

    some changes

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18e03d29854b256953d5987ee5b915e2.js"
+  "/precache-manifest.7a887d9c9a25b177a1c6617584f71bce.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
