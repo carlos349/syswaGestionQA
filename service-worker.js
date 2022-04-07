9e073e38712266b01658f15ce6436a1a7a273af1
carlos capacho <carlos.gomes349@gmail.com>
2022-04-07 17:28:05 -0400

    fix bug vlid online

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a18a6c890f53e35ee69f4e4b1d8b9122.js"
+  "/precache-manifest.b6ce7ce984a6fd7a4ab91efdf9c31893.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
